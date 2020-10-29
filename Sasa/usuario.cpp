5ab2eac1b6337275b8721336a336dd2e9207d4f6
Maximiliano <maxirpc2607@gmail.com>
2020-10-29 15:48:51 -0300

    .

--- Sasa/usuario.cpp
+++ Sasa/usuario.cpp
@@ -96,6 +96,7 @@ bool Usuario::leerDisco(int pos){
 	p=fopen("datos/usuario.dat","rb");
 	if(p==NULL){
 		cout<<"No existe el archivo";
+		return false;
     }
 	fseek(p,pos*sizeof *this,0);
 	x=fread(this,sizeof *this,1,p);
