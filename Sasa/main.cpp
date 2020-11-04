e00d5f909e3b7cffb65965e4517845b603cd3b4b
Maximiliano <maxirpc2607@gmail.com>
2020-11-04 13:30:55 -0300

    fix
    Se soluciona mostrar por pagina

--- Sasa/main.cpp
+++ Sasa/main.cpp
@@ -20,28 +20,36 @@ int main()
     Producto pr;
     int cant = cantProd();
     int pos=0, productos = 5;
+    int paginas= cant / productos;
     int resp=1;
     while(resp != 0){
-        FILE *p;
+        /*FILE *p;
         p=fopen("datos/producto.dat","rb");
         if(p==NULL){
             cout<<"No existe el archivo";
             return -1;
-        }
+        }*/
+        rlutil::cls();
+        cout << "-----------------------------" << endl;
+        cout << "TOTAL DE PRODUCTOS: " << cant << endl;
+        cout << "-----------------------------" << endl;
         for(pos; pos < productos; pos++){
-
-            fseek(p,pos*sizeof(Producto),0);
-            fread(&pr,sizeof(Producto),1,p);
+            //fread(&pr,sizeof(Producto),1,p);
+            pr.leerDisco(pos);
             pr.mostrar(2);
 
         }
-        fclose(p);
-        cout << endl;
-        cout << "0- SALIR 1- PAGINA SIGUIENTE" << endl;
+        //fseek(p,0,0);
+        //fseek(p,pos*sizeof(Producto),0);
+        //fclose(p);
+        cout << "-----------------------------" << endl;
+        cout << "PAGINAS: " << paginas << endl;
+        cout << "0- SALIR 1- PAGINA SIGUIENTE: > ";
         cin >> resp;
+        cout << "-----------------------------" << endl;
         if(resp != 0){
-            pos+5;
-            productos+5;
+            //pos+5;
+            productos+=5;
         }
     }
     //return 0;
