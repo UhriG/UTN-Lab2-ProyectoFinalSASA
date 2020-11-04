fc08c48d07d9ce542053d3f65e795c342ee6d2ee
Maximiliano <maxirpc2607@gmail.com>
2020-11-04 01:05:48 -0300

    Paginacion
    Seguir probando variantes

--- Sasa/main.cpp
+++ Sasa/main.cpp
@@ -10,12 +10,39 @@ using namespace std;
 #include "producto.h" // BORRAR DESPUES DE LA PRUEBA
 int main()
 {
-    initUI();
+    //initUI();
 
-    compAdmin();
+    //compAdmin();
     //menuLogin();
 
     //INGRESAR LAS FUNCIONES PARA PROBAR, COMENTANDO EL MENUPRINCIPAL
-    menuPrincipal();
+    //menuPrincipal();
+    Producto pr;
+    int cant = cantProd();
+    int pos=0, productos = 5;
+    int resp=1;
+    while(resp != 0){
+        FILE *p;
+        p=fopen("datos/producto.dat","rb");
+        if(p==NULL){
+            cout<<"No existe el archivo";
+            return -1;
+        }
+        for(pos; pos < productos; pos++){
+
+            fseek(p,pos*sizeof(Producto),0);
+            fread(&pr,sizeof(Producto),1,p);
+            pr.mostrar(2);
+
+        }
+        fclose(p);
+        cout << endl;
+        cout << "0- SALIR 1- PAGINA SIGUIENTE" << endl;
+        cin >> resp;
+        if(resp != 0){
+            pos+5;
+            productos+5;
+        }
+    }
     //return 0;
 }
